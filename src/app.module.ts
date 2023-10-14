c94cf9c00b424a4c5056b50e46587ff499313900
iqbal89co <iqbalfakhriza089@gmail.com>
2023-10-14 22:21:53 +0700

    database: connect to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      // host: '172.21.0.2',
+      host: 'localhost',
+      port: 5432,
+      username: 'iqbal123',
+      password: 'pass123',
+      database: 'learn_nest',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
